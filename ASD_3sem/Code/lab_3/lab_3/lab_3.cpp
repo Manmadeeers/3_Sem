39cb0c8e6598085ee3326b6e87ac0f6cebbc1ee6
Manmad <philmade6@gmail.com>
2024-09-23 19:11:49 +0300

    ASD lab3 finished

--- ASD_3sem/Code/lab_3/lab_3/lab_3.cpp
+++ ASD_3sem/Code/lab_3/lab_3/lab_3.cpp
@@ -1,25 +1,124 @@
 ﻿//<-------------------->
-//Deikstra's algorythm for graph where each edge has a weight
+//Dijkstra's algorythm for graph where each edge has a weight
+// for convinience the graph itself will be implemented as an adjacency matrix
 // 
-// 
-//<-------------------->
+//<--------------------->
 #include <iostream>
+#include <vector>
 #define INF INT_MAX
 using namespace std;
 
-const int NodesInGraph = 9;				// A  B  C  D  E  F  G  H  I
-int Graph[NodesInGraph][NodesInGraph] = { {0, 7, 10,0 ,0 ,0, 0, 0, 0},//A
-										  {7, 0, 0, 0, 0, 9, 27,0, 0},//B
-										  {10,0, 0, 0, 31,8, 0, 0, 0},//C
-										  {0, 0, 0, 0, 32,0, 0, 17,21},//D
-										  {0, 0, 31,32,0, 0, 0, 0, 0},//E
-										  {0, 9, 8, 0, 0, 0, 0, 11,0},//F
-										  {0, 27,0, 0, 0, 0, 0, 0, 15},//G
-										  {0, 0, 0, 17,0, 11,0, 0, 15},//H
-										  {0, 0, 0, 21,0, 0, 15,15, 0}};//I
+const int NodesInGraph = 9;
+vector<vector<int>>AdjMatrix(NodesInGraph, vector<int>(NodesInGraph));
+char Letters[] = { 'A','B','C','D','E','F','G','H','I' };
+vector<int>Distancies(NodesInGraph, INF);
+vector<bool>VisitedNodes(NodesInGraph, false);
+
+void AddNode(char Source, char Dest, int Weight) {
+	//convert char to int decimal
+	int Src_num = Source - 65;
+	int Dest_num = Dest - 65;
+
+	AdjMatrix[Src_num][Dest_num] = Weight;
+	AdjMatrix[Dest_num][Src_num] = Weight;
+}
+
+void PrintGraph() {
+	cout << "Adjacency matrix: " << endl;
+	cout << '\t';
+	for (int l = 0; l < 9; l++) {
+		cout<<'\t' << Letters[l] << ' ';
+	}
+	cout << endl;
+
+	for (int i = 0; i < NodesInGraph; i++) {
+		cout<<'\t';
+		for (int j = 0; j < NodesInGraph; j++) {
+			cout<<'\t' << AdjMatrix[i][j] << ' ';
+		}
+		cout<<'\t' << Letters[i];
+		cout << endl;
+	}
+	cout << endl << endl;
+}
+
+int findMinDist() {
+	int MIN = INT_MAX;
+	int MinIndex;
+
+	for (int i = 0; i < NodesInGraph; i++) {
+		if (!VisitedNodes[i] && Distancies[i] <= MIN) {
+			MIN = Distancies[i];
+			MinIndex = i;
+		}
+	}
+	return MinIndex;
+}
+
+void PrintDijkstra(int start) {
+	cout << "Solution:" << endl;
+	for (int i = 0; i < NodesInGraph; i++) {
+		if (i != start) {
+			cout << "Length of the shortest path from " << Letters[start] << " to " << Letters[i] << " is " << Distancies[i] << endl;
+		}
+	}
+}
+
+void Dijkstra(char StartVertex) {
+	int Start = StartVertex - 65;
+	Distancies[Start] = 0;
+
+	for (int i = 0; i < NodesInGraph - 1; i++) {
+		int U = findMinDist();
+
+		VisitedNodes[U] = true;
+
+		//check all the nodes 
+		for (int i = 0; i < NodesInGraph; i++) {
+			//if the current node is:
+			//Not visited yet
+			// it has connection with the minimal path to it 
+			// the U node already has initialized distance in Distancies array
+			// distance to this node added to the distance of connection between this note and current is smaller then 
+			// Minimal distance to this node
+			//
+			if (!VisitedNodes[i] && AdjMatrix[U][i]!=0 && Distancies[U] != INF && Distancies[U] + AdjMatrix[U][i] < Distancies[i]) {
+				//set the minimal distance from start node to this current node to the new meaning
+				Distancies[i] = Distancies[U] + AdjMatrix[U][i];
+			}
+		}
+	}
+
+	PrintDijkstra(Start);
+}
 
 
 void main()
 {
+	cout << "Implementing the graph" << endl;
+	AddNode('A', 'B', 7);
+	AddNode('A', 'C', 10);
+	AddNode('B', 'F', 9);
+	AddNode('B', 'G', 27);
+	AddNode('C', 'F', 8);
+	AddNode('C', 'E', 31);
+	AddNode('F', 'H', 11);
+	AddNode('E', 'D', 32);
+	AddNode('D', 'H', 17);
+	AddNode('D', 'I', 21);
+	AddNode('H', 'I', 15);
+	AddNode('G', 'I', 15);
+
+	PrintGraph();
+
+	char StartVertex;
+	cout << "Enter the start vertex for dijkstra algorithm: ";
+	cin >> StartVertex;
+	while (!(StartVertex >= 'A' && StartVertex <= 'I')) {
+		cout << "Enter the correct letter: ";
+		cin >> StartVertex;
+	}
+
+	Dijkstra(StartVertex);
 
 }
