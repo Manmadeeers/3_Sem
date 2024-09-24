a85a4cdaf88693f884ff898254301aafcf81c8f3
Manmad <philmade6@gmail.com>
2024-09-24 16:20:24 +0300

    KPO lab16 in dev. new finit automata graph neede

--- KPO_3sem/Code/lab_16/lab_16/FST.h
+++ KPO_3sem/Code/lab_16/lab_16/FST.h
@@ -1,61 +1,47 @@
 #pragma once
-#include "stdafx.h"
+#include<iostream>
+#include<cstdlib>
+using namespace std;
 
-namespace FST {
-	//edge: symbol -> vertex of a transition graph
-	struct RELATION {
+#define PROC 'P'
+#define BLANK ' '
+#define LAMBDA 'L'
+#define END 'E'
+#define START 'S'
+#define COS 'C'
+#define SIN 'I'
+
+
+namespace FST
+{
+	struct RELATION
+	{
 		char symbol;
 		short nnode;
-		RELATION(char c = 0x00, short ns = NULL);
+		RELATION(
+			char c = 0x00,
+			short ns = 0
+		);
 	};
-	//constructor
-	RELATION::RELATION(char c, short ns) {
-		symbol = c;
-		nnode = ns;
-	}
-	//vertex of a transition graph
-	struct NODE {
+
+	struct NODE
+	{
 		short n_relation;
 		RELATION* relations;
 		NODE();
 		NODE(short n, RELATION rel, ...);
 	};
-	NODE::NODE() {
-		n_relation = 0;
-		RELATION* relations = NULL;
-	}
-	NODE::NODE(short n, RELATION rel, ...) {
-		n_relation = n;
-		RELATION* pointer = &rel;
-		relations = new RELATION[n];
-		for (short i = 0; i < n; i++) {
-			relations[i] = pointer[i];
-		}
-	}
-	//undetermined finit automata
-	struct FST {
+
+	struct FST
+	{
 		char* chain;
 		short position;
 		short nstates;
 		NODE* nodes;
 		short* rstates;
-		FST(char* ch, short ns, NODE n, ...);
+		FST(char* s, short ns, NODE n, ...);
 	};
-	FST::FST(char* ch, short ns, NODE n, ...) {
-		chain = ch;
-		nstates = ns;
-		nodes = new NODE[ns];
-		NODE* pointer = &n;
-		for (short k = 0; k < ns; k++) {
-			nodes[k] = pointer[k];
-		}
-		rstates = new short[nstates];
-		memset(rstates, 0xff, sizeof(short) * nstates);
-		rstates[0] = 0;
-		position = -1;
-	}
 
-	//execute chain determination
 	bool execute(FST& fst);
 	bool step(FST& fst, short*& rstates);
-};
+}
--- KPO_3sem/Code/lab_16/lab_16/lab_16.cpp
+++ KPO_3sem/Code/lab_16/lab_16/lab_16.cpp
@@ -1,22 +1,39 @@
-#include "stdafx.h"
-#include <vector>
+#include "FST.h"
 
 void main()
 {
-	char line1[] = {PROC, BLANK, LAMBDA ,BLANK, END};
-	FST::FST fst1(line1,
-		5,
-		FST::NODE(1, FST::RELATION(PROC, 1)),
-		FST::NODE(1, FST::RELATION(BLANK, 2)),
-		FST::NODE(1, FST::RELATION(LAMBDA, 3)),
-		FST::NODE(1, FST::RELATION(BLANK, 4)),
-		FST::NODE(1,FST::RELATION(END,5)),
-		FST::NODE()
-		);
+	setlocale(LC_ALL, "rus");
+	char string1[]{ PROC,BLANK,LAMBDA,BLANK ,END };
+	FST::FST fst1(
+		string1,
+		4,
+		FST::NODE(1,FST::RELATION(PROC,1)),
+		FST::NODE(1,FST::RELATION(BLANK,2)),
+		FST::NODE(2,FST::RELATION(BLANK,2),FST::RELATION(),
+
+	);
 	if (FST::execute(fst1)) {
-		cout << "Chain " << fst1.chain << " has been determined" << endl;
+		cout << "Цепочка " << fst1.chain<< " распознана" << endl;
 	}
-	else {
-		cout << "Chain " << fst1.chain << " has not been determined" << endl;
+	else
+	{
+		cout << "Цепочка " << fst1.chain << " не распознана" << endl;
+	}
+	char string2[]{ "afbccp" };
+	FST::FST fst2(
+		string2,
+		6,
+		FST::NODE(1, FST::RELATION('a', 1)),
+		FST::NODE(2, FST::RELATION('f', 1), FST::RELATION('b', 2)),
+		FST::NODE(3, FST::RELATION('f', 2), FST::RELATION('c', 3), FST::RELATION('r', 3)),
+		FST::NODE(4, FST::RELATION('r', 3), FST::RELATION('c', 3), FST::RELATION('f', 4), FST::RELATION('d', 5)),
+		FST::NODE(2, FST::RELATION('f', 4), FST::RELATION('d', 5)),
+		FST::NODE());
+	if (FST::execute(fst2)) {
+		cout << "Цепочка " << fst2.chain << " распознана" << endl;
+	}
+	else
+	{
+		cout << "Цепочка " << fst2.chain << " не распознана" << endl;
 	}
 }
