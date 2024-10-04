7f3185eba2a64613784bc1696adb945d0cbb40ca
Manmad <philmade6@gmail.com>
2024-10-04 14:22:50 +0300

    ASD lab5 started and some materials added

---
+++ ASD_3sem/Code/lab_5/lab_5/lab_5.cpp
@@ -0,0 +1,15 @@
+﻿//<-------------------->
+// finding Minimum Spanning Tree (MST) via different algorythms
+// 
+//<-------------------->
+#include <iostream>
+#include <vector>
+using namespace std;
+const int NodesInGraph = 8;
+vector<vector<int>>AdjMatrix(NodesInGraph, vector<int>(NodesInGraph));
+
+
+void main()
+{
+
+}
