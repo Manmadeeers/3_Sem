06d27666b4964e05751423755de7d994a75dee02
Manmad <philmade6@gmail.com>
2024-10-15 15:52:07 +0300

    OOP lab3 done and some matereals added

---
+++ OOP_3sem/Code/lab3/lab3/Classes.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public partial class Password
+    {
+        private string _password;
+        private int _length;
+        private const string _default = "1234pass";
+        private const int _defaultLenght = 8;
+        private const int _maxLength = 12;
+
+        public int MaxLength
+        {
+            get { return _maxLength; }
+        }
+        public string Default
+        {
+            get { return _default; }
+        }
+        public int DefLength
+        {
+            get { return _defaultLenght; }
+        }
+
+        public string Pass
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+        public int Length
+        {
+            get { return _length; }
+        }
+
+
+
+        public Password() { }
+        public Password(string password)
+        {
+            if (String.IsNullOrEmpty(password))
+            {
+                throw new Exception("Password could not be initialized with null or empty string");
+            }
+            _password = password;
+            _length = password.Length;
+        }
+
+
+        public void PrintPass()
+        {
+            Console.Write("Shhhhh, don't tell anybody: ");
+            Console.Write(_password);
+            Console.WriteLine();
+        }
+
+
+
+        public static bool operator >(Password pass1, Password pass2)
+        {
+            if (pass1._length > pass2._length)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public static bool operator <(Password pass1, Password pass2)
+        {
+            if (pass1._length < pass2._length)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator !=(Password pass1, Password pass2)
+        {
+            if (!pass1._password.Equals(pass2._password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator ==(Password pass1, Password pass2)
+        {
+            if (pass1._password.Equals(pass2._password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static Password operator ++(Password pass)
+        {
+            
+            pass._password = pass.Default;
+            pass._length = pass.DefLength;
+            return pass;
+
+        }
+        public static Password operator --(Password pass)
+        {
+            pass._password = "nullpass";
+            pass._length = 8;
+            return pass;
+        }
+
+        
+        public static bool operator true(Password pass)
+        {
+
+            if (pass._length >= 10 && (pass._password.Contains('!') || pass._password.Contains('$')))
+            {
+                return true; 
+            }
+            return false;
+        }
+
+        public static bool operator false(Password pass)
+        {
+            if(!(pass._length >= 10 && (pass._password.Contains('!') || pass._password.Contains('$'))))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static Password operator -( Password pass,char item)
+        {
+            if (item.ToString() == "")
+            {
+                throw new ArgumentException("Could not substract null from a password");
+            }
+             string modifiedPassword =pass._password.Substring(0,pass._length - 1);
+            modifiedPassword += item;
+            pass._password = modifiedPassword;
+            return pass;
+        }
+    }
+
+    internal static class StaticOperation
+    {
+        public static char GetMiddleSymbol(this string str)
+        {
+            if (String.IsNullOrEmpty(str))
+            {
+                throw new Exception("Impossible to find a middle symbol in a null or empty string");
+            }
+            char middle;
+            int middle_position = str.Length / 2;
+            middle = str[middle_position];
+            return middle;
+        }
+
+        public static bool CheckPassLength(this string pass,int minLength,int maxLength)
+        {
+            if (String.IsNullOrEmpty(pass))
+            {
+                throw new Exception("Can not check the validity of a null or empty string");
+            }
+            if (pass.Length >= minLength && pass.Length <= maxLength)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ OOP_3sem/Code/lab3/lab3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Password pass1 = new Password("1234!$strongpassihope");
+            Password pass2 = new Password("Weakpass");
+            pass1.PrintPass();
+            pass2.PrintPass();
+            Console.WriteLine($"{pass1.Pass} > {pass2.Pass}?: {pass1 > pass2}. And {pass1.Pass} < {pass2.Pass}?: {pass1<pass2}");
+
+            pass2++;
+            Console.WriteLine($"Result of ++ operator: {pass2.Pass}");
+            pass2--;
+            Console.WriteLine($"Result of -- operator: {pass2.Pass}");
+
+            pass1 = pass1 - 'E';
+            Console.WriteLine($"Testing - operator: {pass1.Pass}");
+
+            Console.WriteLine($"Result of {pass1.Pass} != {pass2.Pass} : {pass1 != pass2}");
+            Console.WriteLine($"Result of {pass1.Pass} == {pass2.Pass} : {pass1 == pass2}");
+
+
+            Console.WriteLine($"Checking extensions: {StaticOperation.GetMiddleSymbol(pass1.Pass)}");
+            Console.WriteLine($"Also checking extensions: {StaticOperation.CheckPassLength(pass1.Pass,pass1.MaxLength,pass1.DefLength)}");
+
+        }
+    }
+}
