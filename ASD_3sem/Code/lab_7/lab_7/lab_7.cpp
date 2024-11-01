a5d72a5b43047c5cda1a9781bb1e3006030d2e15
Manmad <philmade6@gmail.com>
2024-11-01 16:39:42 +0300

    ASD lab_7 started

---
+++ ASD_3sem/Code/lab_7/lab_7/lab_7.cpp
@@ -0,0 +1,63 @@
+﻿//<-------------------->
+//
+// finding a maximum subsequence from a sequence
+// 
+//<-------------------->
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+//<-----utility functions----->
+void printSequence(vector<int>numbers) {
+	for (int i = 0; i < numbers.size(); i++) {
+		if (i != numbers.size() - 1) {
+			cout << numbers[i] << ',';
+		}
+		else {
+			cout << numbers[i];
+		}
+
+
+	}
+	cout << endl<<endl;
+}
+bool sortSequence(int first, int second) {
+	return first < second;
+}
+void getSequence(string sequence,vector<int>&numbers) {
+	string tmp = "";
+	for (int i = 0; i < sequence.length(); ++i) {
+
+
+		tmp += sequence[i];
+		if (sequence[i] == ','|| i == sequence.length() - 1) {
+			numbers.push_back(stoi(tmp));
+			tmp = "";
+			continue;
+		}
+	}
+}
+
+void findMaxSubsequence(string sequence) {
+	vector<int>numbers;
+	getSequence(sequence, numbers);
+	cout << "Sequence: " << endl;
+	printSequence(numbers);
+	sort(numbers.begin(), numbers.end(), sortSequence);
+	cout << "Sorted sequence: " << endl;
+	printSequence(numbers);
+	
+	
+}
+
+void main() {
+
+	string src_sequence;
+	cout << "Enter the source sequence(through coma): ";
+	getline(cin, src_sequence);
+	findMaxSubsequence(src_sequence);
+
+}
