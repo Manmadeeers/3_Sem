cf29c78bad2b38dc77e0455af66c961c847bc8bc
Manmad <philmade6@gmail.com>
2024-11-16 12:19:40 +0300

    SPL lab8 in progress

---
+++ SPL_3sem/Code/lab_8/script.js
@@ -0,0 +1,106 @@
+//TASK 1
+let user = {
+    name: 'Masha',
+    age: 21
+};
+let user_copy ={...user};
+
+let numbers = [1, 2, 3];
+let numbers_copy = [...numbers];
+
+let user1 = {
+    name: 'Masha',
+    age: 23,
+    location: {
+        city: 'Minsk',
+        country: 'Belarus'
+    }
+};
+console.log(user1);
+let user1_copy = {...user1,location:{...user1.location}};
+console.log(user1_copy);
+
+let user2 = {
+    name: 'Masha',
+    age: 28,
+    skills: ["HTML", "CSS", "JavaScript", "React"]
+};
+let user2_copy = {...user2,skills:{...user2.skills}};
+
+const array = [
+    { id: 1, name: 'Vasya', group: 10 },
+    { id: 2, name: 'Ivan', group: 11 },
+    { id: 3, name: 'Masha', group: 12 },
+    { id: 4, name: 'Petya', group: 10 },
+    { id: 5, name: 'Kira', group: 11 },
+]
+const array_copy = [...array];
+//array_copy[0] = {...array[0]};
+
+let user4 = {
+    name: 'Masha',
+    age: 19,
+    studies: {
+        university: 'BSTU',
+        speciality: 'designer',
+        year: 2020,
+        exams: {
+            maths: true,
+            programming: false
+        }
+    }
+};
+let user4_copy = {...user4};
+
+let user5 = {
+    name: 'Masha',
+    age: 22,
+    studies: {
+        university: 'BSTU',
+        speciality: 'designer',
+        year: 2020,
+        department: {
+            faculty: 'FIT',
+            group: 10,
+        },
+        exams: [
+            { maths: true, mark: 8 },
+            { programming: true, mark: 4 },
+        ]
+    }
+};
+let user5_copy = {...user5};
+
+let user6 = {
+    name: 'Masha',
+    age: 21,
+    studies: {
+        university: 'BSTU',
+        speciality: 'designer',
+        year: 2020,
+        department: {
+            faculty: 'FIT',
+            group: 10,
+        },
+        exams: [
+            {
+                maths: true,
+                mark: 8,
+                professor: {
+                    name: 'Ivan Ivanov ',
+                    degree: 'PhD'
+                }
+            },
+            {
+                programming: true,
+                mark: 10,
+                professor: {
+                    name: 'Petr Petrov',
+                    degree: 'PhD'
+                }
+            },
+        ],
+    }
+};
+let user6_copy = {...user6};
+//TASK 2
