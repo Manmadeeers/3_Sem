502f4b9ebbdcb3e421caf1213827f7a1f39f1be0
Manmad <philmade6@gmail.com>
2024-11-28 21:03:57 +0300

    KPO 1st Assembler done and some materials added

---
+++ KPO_3sem/Code/lab1_asm/lab1_asm/Program.asm
@@ -0,0 +1,30 @@
+.586P
+.MODEL FLAT, STDCALL
+includelib kernel32.lib
+
+ExitProcess PROTO:DWORD
+MessageBoxA PROTO:DWORD,:DWORD,:DWORD,:DWORD
+
+.STACK 4096
+
+.CONST
+
+.DATA
+
+MB_OK EQU 0
+STR1 DB "My first ASM program",0
+STR2 DB "Hi everyone!",0
+HW DD ?
+
+.CODE
+
+main PROC
+START:
+		
+	INVOKE MessageBoxA, HW, OFFSET STR2, OFFSET STR1, MB_OK
+
+	push 0
+	call ExitProcess
+main ENDP
+
+end main
