43d9b6fe2cdbea1ad86c6c7d4cd9084267e7b69c
Manmad <philmade6@gmail.com>
2024-12-06 16:03:27 +0300

    ASD lab9 in progress

--- ASD_3sem/Code/lab_9/lab_9/lab_9.cpp
+++ ASD_3sem/Code/lab_9/lab_9/lab_9.cpp
@@ -54,11 +54,16 @@ void printGrph(vector<vector<int>>graph) {
 }
 
 void addEdge(int from, int to, int weight,vector<vector<int>>&graph) {
-	graph[from][to] = weight;
+	graph[from-1][to-1] = weight;
 }
 
 void deleteEdge(int from, int to, vector<vector<int>>& graph) {
-	graph[from][to] = INF;
+	graph[from-1][to-1] = INF;
+}
+
+int calculateDistance(vector<int>path,vector< ) {
+	int distance = 0;
+	for(int i=0;i<)
 }
 
 
@@ -90,6 +95,85 @@ void main() {
 	implementGraph(graph, CITIES);
 	printGrph(graph);
 
-	 
-	
+	addEdge(1, 2, 10, graph);
+	addEdge(2, 1, 8, graph);
+	addEdge(1, 3, 15, graph);
+	addEdge(3, 1, 7, graph);
+	addEdge(1, 4, 32, graph);
+	addEdge(4, 1, 16, graph);
+	addEdge(1, 5, 16, graph);
+	addEdge(5, 1, 10, graph);
+	addEdge(1, 6, 31, graph);
+	addEdge(6, 1, 32, graph);
+	addEdge(1, 7, 5, graph);
+	addEdge(7, 1, 13, graph);
+	addEdge(1, 8, 6, graph);
+	addEdge(8, 1, 11, graph);
+	addEdge(1, 9, 4, graph);
+	addEdge(9, 1, 12, graph);
+
+	addEdge(2, 3, 4, graph);
+	addEdge(3, 2, 6, graph);
+	addEdge(2, 4, 21, graph);
+	addEdge(4, 2, 17, graph);
+	addEdge(2, 5, 16, graph);
+	addEdge(5, 2, 19, graph);
+	addEdge(2, 6, 36, graph);
+	addEdge(6, 2, 40, graph);
+	addEdge(2, 7, 8, graph);
+	addEdge(7, 2, 17, graph);
+	addEdge(2, 8, 12, graph);
+	addEdge(8, 2, 8, graph);
+	addEdge(2, 9, 9, graph);
+	addEdge(9, 2, 3, graph);
+
+	addEdge(3, 4, 4, graph);
+	addEdge(4, 3, 6, graph);
+	addEdge(3, 5, 11, graph);
+	addEdge(5, 3, 8, graph);
+	addEdge(3, 6, 20, graph);
+	addEdge(6, 3, 17,graph);
+	addEdge(3, 7, 12, graph);
+	addEdge(7, 3, 10, graph);
+	addEdge(3, 8, 41, graph);
+	addEdge(8, 3, 40, graph);
+	addEdge(3, 9, 30, graph);
+	addEdge(9, 3, 33, graph);
+
+	addEdge(4, 5, 8, graph);
+	addEdge(5, 4, 11, graph);
+	addEdge(4, 6, 21, graph);
+	addEdge(6, 4, 7, graph);
+	addEdge(4, 7, 42, graph);
+	addEdge(7, 4, 36, graph);
+	addEdge(4, 8, 12, graph);
+	addEdge(8, 4, 20, graph);
+	addEdge(4, 9, 11, graph);
+	addEdge(9, 4, 5, graph);
+
+	addEdge(5, 6, 3, graph);
+	addEdge(6, 5, 11, graph);
+	addEdge(5, 7, 16, graph);
+	addEdge(7, 5, 10, graph);
+	addEdge(5, 8, 31, graph);
+	addEdge(8, 5, 20, graph);
+	addEdge(5, 9, 10, graph);
+	addEdge(9, 5, 11, graph);
+
+	addEdge(6, 7, 10, graph);
+	addEdge(7, 6, 4, graph);
+	addEdge(6, 8, 21, graph);
+	addEdge(8, 6, 15, graph);
+	addEdge(6, 9, 18, graph);
+	addEdge(9, 6, 10, graph);
+
+	addEdge(7, 8, 2, graph);
+	addEdge(8, 7, 10, graph);
+	addEdge(7, 9, 31, graph);
+	addEdge(9, 7, 20, graph);
+
+	addEdge(8, 9, 9, graph);
+	addEdge(9, 8, 18, graph);
+
+	printGrph(graph);
 }
