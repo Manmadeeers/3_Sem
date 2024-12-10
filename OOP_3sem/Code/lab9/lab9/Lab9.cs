be0fc347b7bbf6f99f278c991e004c668fdf0c11
Manmad <philmade6@gmail.com>
2024-12-10 12:47:51 +0300

    OOP lab9 started and some materials added

---
+++ OOP_3sem/Code/lab9/lab9/Lab9.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    public class Service:IOrderedDictionary
+    {
+        public Service() { }
+
+    }
+}
