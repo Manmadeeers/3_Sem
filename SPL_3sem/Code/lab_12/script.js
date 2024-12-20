e4e345f1da00ef113d46a84d2c517e3b64ca65d3
Manmad <philmade6@gmail.com>
2024-12-20 19:16:46 +0300

    SPL lab13 started

--- SPL_3sem/Code/lab_12/script.js
+++ SPL_3sem/Code/lab_12/script.js
@@ -86,6 +86,14 @@ class Sudoku{
         this.printBoard();
     }
 
+    solveSudoku(){
+        
+        for(let i=0;i<9;i++){
+            for(let j=0;j<9;j++){
+
+            }
+        }
+    }
     
 }
 
