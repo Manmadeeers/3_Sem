0f30516952b0b37080573b3c1a967f62468a8157
Manmad <philmade6@gmail.com>
2024-12-27 09:24:32 +0300

    ASD lab 10 ad 11 done

---
+++ ASD_3sem/Code/lab_11/lab_11/lab_11.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+
+bool random_choice(const std::vector<int>& boxes, int prisoner) {
+    int attempts = 50;
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_int_distribution<> random(0, 99);
+    std::vector<int> isopen;
+    int box;
+    for (int i = 0; i < attempts; ++i) {
+        box = random(gen);
+        if (std::find(isopen.begin(), isopen.end(), box) != isopen.end()) {
+            i--;
+            continue;
+        }
+        if (boxes[box] == prisoner) {
+            return true;
+        }
+        isopen.push_back(box);
+    }
+    return false;
+}
+
+bool loop_following(const std::vector<int>& boxes, int prisoner) {
+    int attempts = 50;
+    int next_box = prisoner;
+    for (int i = 0; i < attempts; ++i) {
+        if (boxes[next_box - 1] == prisoner) {
+            return true;
+        }
+        next_box = boxes[next_box - 1];
+    }
+    return false;
+}
+
+int main() {
+    std::locale loc("en_US.UTF-8");
+    std::cout.imbue(loc);
+
+    int rounds;
+    std::cout << "Number of rounds: ";
+    while (std::cin >> rounds && !(rounds > 0));
+
+    int random_success = 0;
+    int loop_success = 0;
+
+    for (int i = 0; i < rounds; ++i) {
+
+        std::vector<int> boxes(100);
+        for (int j = 0; j < 100; ++j) {
+            boxes[j] = j + 1;
+        }
+        std::shuffle(boxes.begin(), boxes.end(), std::mt19937{ std::random_device{}() });
+
+        bool random_all_success = true;
+        for (int prisoner = 1; prisoner <= 100; ++prisoner) {
+            if (!random_choice(boxes, prisoner)) {
+                random_all_success = false;
+                break;
+            }
+        }
+        if (random_all_success) {
+            ++random_success;
+        }
+
+        bool loop_all_success = true;
+        for (int prisoner = 1; prisoner <= 100; ++prisoner) {
+            if (!loop_following(boxes, prisoner)) {
+                loop_all_success = false;
+                break;
+            }
+        }
+        if (loop_all_success) {
+            ++loop_success;
+            std::cout << i << " success\n";
+        }
+
+    }
+
+    std::cout << "\n\nRandom successes: " << random_success << "/" << rounds << ", " << ((float)random_success / (float)rounds) * 100 << "%\n";
+    std::cout << "By loop successes: " << loop_success << "/" << rounds << ", " << ((float)loop_success / (float)rounds) * 100 << "%\n";
+
+    return 0;
+}
