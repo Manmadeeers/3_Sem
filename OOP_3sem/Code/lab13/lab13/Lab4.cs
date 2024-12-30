18ebd242a629576ee977e907081b412022b97054
Manmad <philmade6@gmail.com>
2024-12-31 00:46:22 +0300

    OOP lab13 started

---
+++ OOP_3sem/Code/lab13/lab13/Lab4.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab13
+{
+    interface IFurniture
+    {
+        void isClean();
+    }
+    public abstract class Product
+    {
+        protected string name;
+        protected bool clean = false;
+        public string Name
+        {
+            get { return name; }
+        }
+
+        protected string price;
+        public string Price
+        {
+            get { return price; }
+        }
+
+        protected Product(string Nname, string Nprice)
+        {
+            if (String.IsNullOrEmpty(Nname) || String.IsNullOrEmpty(Nprice))
+            {
+                throw new Exception("Can't initialize field with null or empty meaning");
+            }
+            name = Nname;
+            price = Nprice;
+        }
+
+        public virtual void Clean()
+        {
+            Console.WriteLine($"{name} is now clean");
+            clean = true;
+        }
+        public abstract void isClean();
+
+        public virtual void Buy()
+        {
+            Console.WriteLine($"You've bought a {name} for {price}");
+        }
+
+
+    }//end of Product
+
+    interface IChair
+    {
+        void Sit();
+    }
+
+    public abstract class Furniture : Product, IFurniture
+    {
+        public Furniture(string name, string price) : base(name, price)
+        {
+
+        }
+        public override string ToString()
+        {
+            return name + " costs  " + price;
+        }
+        public override void isClean()
+        {
+            Console.WriteLine($"This {name} is clean");
+        }
+
+    }
+}
---
+++ OOP_3sem/Code/lab13/lab13/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lab13
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
